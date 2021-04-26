062303f1107bd94eaf2ba75ccb21cb6c917dcac8
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-04-26 15:25:31 +0700

    fix decision tree model pickel

--- iris_decission_tree.py
+++ iris_decission_tree.py
@@ -27,7 +27,7 @@ accuracy=metrics.accuracy_score(prediction,test_y)
 print(f"SVM model accuracy is {format(accuracy)}")
 
 # Save to file in the current working directory
-pkl_filename = "iris_log_reg.pkl"
+pkl_filename = "iris_decision_tree.pkl"
 with open(pkl_filename, 'wb') as file:
     pickle.dump(model, file)
 
