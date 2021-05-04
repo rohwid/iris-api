8de23292c4a19db233e055093754420e9b2c6d42
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-05-04 16:12:42 +0700

    rename jupyter, finish compose iris_server and postgres, more CI/CD folder structure

---
+++ docker/postgres/scripts/init-iris-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    DROP DATABASE IF EXISTS iris_db;
+    DROP ROLE IF EXISTS iris;
+    CREATE USER iris;
+    CREATE DATABASE iris_db;
+    GRANT ALL PRIVILEGES ON DATABASE iris_db TO iris;
+    \c iris_db
+    CREATE TABLE CLASS_RESULT(
+        ID INT PRIMARY KEY NOT NULL,
+        METHOD VARCHAR (50) NOT NULL,
+        ACCURACY NUMERIC NOT NULL
+    );
+EOSQL
