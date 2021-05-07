f797fc6e370d238986374605e3161b00aee2e76f
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-05-07 13:16:53 +0700

    train and serve from csv dataset, retrain, output file

--- ml/iris/iris_logistic_regression.py
+++ ml/iris/iris_logistic_regression.py
@@ -7,12 +7,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm, metrics
 from sklearn.tree import DecisionTreeClassifier
 
-dataset = load_iris()
-data = pd.DataFrame(dataset['data'], columns = ["Petal length","Petal Width","Sepal Length","Sepal Width"])
-data['Species'] = dataset['target']
-data['Species'] = data['Species'].apply(lambda x: dataset['target_names'][x])
-
-train, test = train_test_split(data, test_size = 0.3)
+train = train = pd.read_csv('../datasets/data_train1.csv')
+test = test = pd.read_csv('../datasets/data_test1.csv')
 
 train_X = train[["Sepal Length", "Sepal Width", "Petal length", "Petal Width"]]
 train_y = train.Species
--- ml/iris/load_and_retrain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python ./iris_svm.py
-python ./iris_logistic_regression.py
-python ./iris_decission_tree.py
-
-python ./iris_server.py
