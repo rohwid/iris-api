0569e7c3f7f88ebc6db9ea7ef6b7ac599178ddf4
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2021-06-03 15:12:47 +0700

    move to flask_restful, docker-compose tested

--- ml/iris/iris_server.py
+++ ml/iris/iris_server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
@@ -10,6 +11,7 @@ import pandas as pd
 import random
 
 app = Flask(__name__)
+api = Api(app)
 
 train = train = pd.read_csv('../datasets/data_train1.csv')
 test = test = pd.read_csv('../datasets/data_test1.csv')
@@ -32,8 +34,8 @@ with open('model/iris_decission_tree.pkl', 'rb') as file:
 output_file = '../output/output.json'
 
 def json_converter(json_data):
-    if isinstance(json_data, datetime.datetime):
-        return json_data.__str__()
+        if isinstance(json_data, datetime.datetime):
+            return json_data.__str__()
 
 def write_json(input_data, flag):
     if (flag == 0):
@@ -63,61 +65,63 @@ def write_output(input_data):
     else:
         write_json(input_data, 0)
 
-@app.route('/testing', methods=['POST'])
-def predict():
-    request_data = request.get_json()
+class Testing(Resource):
+    def post(self):
+        request_data = request.get_json()
 
-    data_testing = {
-        'Sepal Length': request_data['Sepal Length'],
-        'Sepal Width': request_data['Sepal Width'],
-        'Petal Length': request_data['Petal Length'],
-        'Petal Width': request_data['Petal Width']
-    }
+        data_testing = {
+            'Sepal Length': request_data['Sepal Length'],
+            'Sepal Width': request_data['Sepal Width'],
+            'Petal Length': request_data['Petal Length'],
+            'Petal Width': request_data['Petal Width']
+        }
 
-    svm_score = svm_pickle_model.score(test_X, test_y)
-    lr_score = lr_pickle_model.score(test_X, test_y)
-    dt_score = dt_pickle_model.score(test_X, test_y)
+        svm_score = svm_pickle_model.score(test_X, test_y)
+        lr_score = lr_pickle_model.score(test_X, test_y)
+        dt_score = dt_pickle_model.score(test_X, test_y)
 
-    svm_predict = svm_pickle_model.predict(pd.DataFrame(data_testing))
-    lr_predict = lr_pickle_model.predict(pd.DataFrame(data_testing))
-    dt_predict = dt_pickle_model.predict(pd.DataFrame(data_testing))
+        svm_predict = svm_pickle_model.predict(pd.DataFrame(data_testing))
+        lr_predict = lr_pickle_model.predict(pd.DataFrame(data_testing))
+        dt_predict = dt_pickle_model.predict(pd.DataFrame(data_testing))
 
-    model_score = [svm_score, lr_score, dt_score]
-    max_score = max(model_score)
+        model_score = [svm_score, lr_score, dt_score]
+        max_score = max(model_score)
 
-    result = [i for i, j in enumerate(model_score) if j == max_score]
+        result = [i for i, j in enumerate(model_score) if j == max_score]
 
-    if (len(result) > 1):
-        select = random.choice(result)
-    else:
-        select = result[0]
-    
-    test_result = {
-        'timestamp': json_converter(datetime.datetime.now()),
-        'method': "NaN",
-        'accuracy': 0
-    }
-    
-    if (select == 0):
-        test_result['method'] = "SVM"
-        test_result['accuracy'] = svm_score
-        write_output(test_result)
+        if (len(result) > 1):
+            select = random.choice(result)
+        else:
+            select = result[0]
         
-        return jsonify(test_result)
-    
-    if (select == 1):
-        test_result['method'] = "Linear Logistic"
-        test_result['accuracy'] = lr_score
-        write_output(test_result)
+        test_result = {
+            'timestamp': json_converter(datetime.datetime.now()),
+            'method': "NaN",
+            'accuracy': 0
+        }
         
-        return jsonify(test_result)
-    
-    if (select == 2):
-        test_result['method'] = "Decision Tree"
-        test_result['accuracy'] = dt_score
-        write_output(test_result)
+        if (select == 0):
+            test_result['method'] = "SVM"
+            test_result['accuracy'] = svm_score
+            write_output(test_result)
+            
+            return jsonify(test_result)
         
-        return jsonify(test_result)
-    
+        if (select == 1):
+            test_result['method'] = "Linear Logistic"
+            test_result['accuracy'] = lr_score
+            write_output(test_result)
+            
+            return jsonify(test_result)
+        
+        if (select == 2):
+            test_result['method'] = "Decision Tree"
+            test_result['accuracy'] = dt_score
+            write_output(test_result)
+            
+            return jsonify(test_result)
+
+api.add_resource(Testing, '/testing')
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True, port = '5000')
